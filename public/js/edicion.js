1be7fc4ba3defc6758caefccc126ad7057dd4eda
Erick <erick.santillan@epn.edu.ec>
2024-01-10 02:24:33 -0500

    Estilos y retoque de paguinas

--- public/js/edicion.js
+++ public/js/edicion.js
@@ -1,3 +1,6 @@
+document.getElementById('cerrarSesionBtn').addEventListener('click', function() {
+  window.location.href = '/index_productos.html';
+});
 document.addEventListener("DOMContentLoaded", () => {
   const productosDiv = document.querySelector(".catalogo");
   const pageList = document.querySelector(".page-list");
